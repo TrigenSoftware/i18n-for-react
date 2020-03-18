ca09eeeaab09777004e29916229b6bcc215148f3
dangreen <danon0404@gmail.com>
2020-03-18 13:13:12 +0700

    rollup config mod

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,12 @@ import {
 	external
 } from '@trigen/scripts-plugin-rollup/helpers';
 import tslint from 'rollup-plugin-tslint';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import babel from 'rollup-plugin-babel';
-import { DEFAULT_EXTENSIONS } from '@babel/core';
+import {
+	DEFAULT_EXTENSIONS
+} from '@babel/core';
 import pkg from './package.json';
 
 const plugins = [
